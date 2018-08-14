aa8d192f407fab213df778b7cec40e13d3f5e88d
luciano.cassettai <luciano.cassettai@gmail.com>
2018-08-14 19:39:34 -0300

    Se cambio el dashboard de inicio

--- application/views/inicio_view.php
+++ application/views/inicio_view.php
@@ -1,54 +1,52 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-
 <div class="row">
-	<div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Usuarios</span>
-              <span class="info-box-number"><?php echo $cant_usuarios; ?></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-blue"><i class="glyphicon glyphicon-education"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Frases</span>
-              <span class="info-box-number">0</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="glyphicon glyphicon-user"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Autores</span>
-              <span class="info-box-number">0</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-list"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Categorias</span>
-              <span class="info-box-number"> <?php echo $cant_categorias;?> </span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
+  <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="small-box bg-aqua">
+      <div class="inner">
+        <span class="info-box-number"><h3><?php echo $cant_usuarios; ?></h3></span>
+        <p>Usuarios</p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-users"></i>
+      </div>
+    </div>
+    <!-- /.info-box -->
+  </div>
+  <div class="col-md-3 col-sm-6 col-xs-12">
+   <div class="small-box bg-green">
+      <div class="inner">
+        <span class="info-box-number"><h3>0</h3></span>
+        <p>Frases</p>
+      </div>
+      <div class="icon">
+        <i class="glyphicon glyphicon-education"></i>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-3 col-sm-6 col-xs-12">
+     <div class="small-box bg-yellow">
+      <div class="inner">
+        <span class="info-box-number"><h3>0</h3></span>
+        <p>Autores</p>
+      </div>
+      <div class="icon">
+        <i class="glyphicon glyphicon-user"></i>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="small-box bg-red">
+      <div class="inner">
+        <span class="info-box-number">
+          <h3><?php echo $cant_categorias;?></h3>
+        </span>
+        <p>Categorias</p>
+      </div>
+      <div class="icon">
+        <i class="glyphicon glyphicon-list"></i>
+      </div>
+    </div>
+  </div>
 </div>
