d859370f24873b8214be702f964f61592018c63b
luciano.cassettai <luciano.cassettai@gmail.com>
2018-08-14 20:54:12 -0300

    Se corrigion archivos de migracion inicial

--- db/migrations/20180726223212_frases_inicial.php
+++ db/migrations/20180726223212_frases_inicial.php
@@ -5,46 +5,37 @@ use Phinx\Migration\AbstractMigration;
 
 class FrasesInicial extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    addCustomColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Any other destructive changes will result in an error when trying to
-     * rollback the migration.
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change()
     {	
-    	$table = $this->table('tipo_usuarios');
-    	$table->addColumn('descripcion','string',array('limit' => 200))
+    	$table = $this->table('perfiles');
+      $table->addColumn('descripcion','string',array('limit' => 200))
+    	     ->addColumn('activo','boolean')
     		    ->create();
+
+      $table = $this->table('modulos');
+      $table->addColumn('descripcion','string',array('limit' => 200))
+            ->create();
 
     	$table = $this->table('usuarios');
         $table->addColumn('usuario', 'string', array('limit' => 200))
               ->addColumn('password', 'string', array('limit' => 1000))
               ->addColumn('nombre', 'string', array('limit' => 200))
               ->addColumn('apellido', 'string', array('limit' => 200))
-              ->addColumn('tipo_usuario','integer')
+              ->addColumn('id_perfil','integer',array('null' => true))
               ->addColumn('activo','boolean')
-              ->addForeignKey('tipo_usuario','tipo_usuarios','id')
+              ->addForeignKey('id_perfil','perfiles','id')
               ->create();
+
+      $table = $this->table('permisos');
+      $table->addColumn('id_modulo','integer')
+            ->addColumn('id_perfil','integer')
+            ->addColumn('crear','boolean')
+            ->addColumn('leer','boolean')
+            ->addColumn('actualizar','boolean')
+            ->addColumn('borrar','boolean')
+            ->addForeignKey('id_modulo','modulos','id')
+            ->addForeignKey('id_perfil','perfiles','id')
+            ->create();
 
     	$table = $this->table('categorias');
         $table->addColumn('nombre', 'string', array('limit' => 100))
@@ -79,36 +70,12 @@ class FrasesInicial extends AbstractMigration
               ->addForeignKey('id_frase', 'frases', 'id')
               ->create();
 
-
-        //-----------------------------------------------------------------------------------
-        //---- Datos Basicos-----------------------------------------------------------------
-        //-----------------------------------------------------------------------------------
-
-        $tipo_usuarios = [
-            [
-              'descripcion'  => 'Administrador'
-            ],
-            [
-              'descripcion'  => 'Participante'
-            ]
-        ];
-
-		$this->table('tipo_usuarios')->insert($tipo_usuarios)->save();
-
-    $tipo_usuarios = [
-            [
-              'id'  => '1',
-              'usuario' => 'lcassettai',
-              'password' => '$2y$10$r060nWfKZviJNCRTWhwVZOb36jYgIHHzm8ZiY8MtCBQ6pwuZPF36a',
-              'nombre' => 'luciano',
-              'apellido' => 'cassettai',
-              'tipo_usuario' => 1,
-              'activo' => 1
-             ]
-        ];
-
-    $this->table('usuarios')->insert($tipo_usuarios)->save();
-
-
+        $table = $this->table('control_usuarios');
+        $table->addColumn('id_usuario', 'integer')
+              ->addColumn('fecha_hora', 'datetime')
+              ->addColumn('operacion', 'string', array('limit' => 100))
+              ->addColumn('detalle', 'string', array('null' => true,'limit' => 300))
+              ->addForeignKey('id_usuario', 'usuarios', 'id')
+              ->create();
     }
 }
---
+++ db/migrations/20180813012619_datos_iniciales.php
@@ -0,0 +1,157 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class DatosIniciales extends AbstractMigration
+{
+    public function up()
+    {
+        $perfiles = [
+              [
+                'id' => 1,
+                'descripcion' => 'Administrador',
+                'activo' => true
+              ],
+              [
+                'id' => 2,
+                'descripcion' => 'Ayudante',
+                'activo' => true
+              ]
+    ];
+
+    $this->table('perfiles')->insert($perfiles)->save();
+
+    $modulos = [
+      [
+        'id' => 1,
+        'descripcion' => 'frases'
+      ],
+      [
+        'id' => 2,
+        'descripcion' => 'autores'
+      ],
+      [
+        'id' => 3,
+        'descripcion' => 'usuarios'
+      ],
+      [
+        'id' => 4,
+        'descripcion' => 'categorias'
+      ],
+      [
+        'id' => 5,
+        'descripcion' => 'perfiles'
+      ]
+    ];
+
+    $this->table('modulos')->insert($modulos)->save();
+
+
+    $permisos = [
+      [
+        'id_modulo' => 1,
+        'id_perfil' => 1,
+        'crear' => true,
+        'leer' => true,
+        'actualizar' => true,
+        'borrar' => true
+      ],
+       [
+        'id_modulo' => 2,
+        'id_perfil' => 1,
+        'crear' => true,
+        'leer' => true,
+        'actualizar' => true,
+        'borrar' => true
+      ],
+       [
+        'id_modulo' => 3,
+        'id_perfil' => 1,
+        'crear' => true,
+        'leer' => true,
+        'actualizar' => true,
+        'borrar' => true
+      ],
+       [
+        'id_modulo' => 4,
+        'id_perfil' => 1,
+        'crear' => true,
+        'leer' => true,
+        'actualizar' => true,
+        'borrar' => true
+      ],
+       [
+        'id_modulo' => 5,
+        'id_perfil' => 1,
+        'crear' => true,
+        'leer' => true,
+        'actualizar' => true,
+        'borrar' => true
+      ],      [
+        'id_modulo' => 1,
+        'id_perfil' => 2,
+        'crear' => false,
+        'leer' => false,
+        'actualizar' => false,
+        'borrar' => false
+      ],
+       [
+        'id_modulo' => 2,
+        'id_perfil' => 2,
+        'crear' => false,
+        'leer' => false,
+        'actualizar' => false,
+        'borrar' => false
+      ],
+       [
+        'id_modulo' => 3,
+        'id_perfil' => 2,
+        'crear' => false,
+        'leer' => false,
+        'actualizar' => false,
+        'borrar' => false
+      ],
+       [
+        'id_modulo' => 4,
+        'id_perfil' => 2,
+        'crear' => false,
+        'leer' => false,
+        'actualizar' => false,
+        'borrar' => false
+      ],
+       [
+        'id_modulo' => 5,
+        'id_perfil' => 2,
+        'crear' => false,
+        'leer' => false,
+        'actualizar' => false,
+        'borrar' => false
+      ]
+    ];
+
+    $this->table('permisos')->insert($permisos)->save();
+
+    $usuario = [
+            [
+              'id'  => 1,
+              'usuario' => 'lcassettai',
+              'password' => '$2y$10$r060nWfKZviJNCRTWhwVZOb36jYgIHHzm8ZiY8MtCBQ6pwuZPF36a',
+              'nombre' => 'luciano',
+              'apellido' => 'cassettai',
+              'id_perfil' => 1,
+              'activo' => 1
+             ]
+        ];
+
+    $this->table('usuarios')->insert($usuario)->save();
+
+    }
+
+    public function down(){
+        $this->execute('DELETE FROM perfiles'); 
+        $this->execute('DELETE FROM modulos'); 
+        $this->execute('DELETE FROM permisos'); 
+        $this->execute('DELETE FROM usuarios'); 
+    }
+}
